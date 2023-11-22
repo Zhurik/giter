f6e9bf9f89401d647219781764aa3bb6b267c4ce
Zhurik <alex15023@yandex.ru>
2023-11-22 18:20:40 +0300

    added storage

---
+++ src/storage/common.rs
@@ -0,0 +1,12 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Clone)]
+
+pub struct Repo {
+    pub name: String,
+    pub url: String,
+}
+
+pub trait Storage {
+    fn list_repos(&self) -> Vec<Repo>;
+}
--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,2 +1,2 @@
-pub mod storage;
+pub mod common;
 pub mod json_storage;
