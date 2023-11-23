2883afbf8f7811fe9968b50a47241540bacb9d3f
Zhuravlev Alexander <amzhuravlev@sbercloud.ru>
2023-11-23 18:37:20 +0300

    added cli params

---
+++ src/config/config.rs
@@ -0,0 +1,10 @@
+use clap::Parser;
+
+/// Simple utility for quickly checking what commit in this pod
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    /// Path to repos.json file
+    #[arg(short, long, default_value_t = String::from("./repos.json"))]
+    pub storage_path: String,
+}
